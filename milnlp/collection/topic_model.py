bf0fe3930275886ae4a468111d49611d80f761f1
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2018-09-04 17:06:39 -0400

    Add ComplexQuery class for performing union and intersection operators. Allow recursive processing of out-of-date queries from shelf when executing higher-level complex queries.  Include basic example notebook.

--- milnlp/collection/topic_model.py
+++ milnlp/collection/topic_model.py
@@ -1,4 +1,6 @@
 import re
+import shelve
+from itertools import chain
 #
 from milnlp.regex.utils import regex_plurals, regex_constituents, regex_delims, regex_whole_word
 
@@ -122,3 +124,100 @@ class SimpleQuery(AbstractQuery):
                 elif pages:
                     self.match[file] = sorted(list(pages))
         self.processed = True
+
+
+class ComplexQuery(AbstractQuery):
+    UUID_INDEX = 1
+
+    def __init__(self, shelf_path, key_list, operator):
+        super().__init__()
+        #
+        self.type = 'complex'
+        self.processed = False
+        self.phrase = None
+        self.operator = operator.lower()  # union or intersection
+        self.shelf_path = shelf_path
+        #
+        self.__set_uuid()
+        # todo set phrase i.e. "union(SQ_001, SQ_002)"
+        self._dependencies = self.__load_objects(key_list)
+        #
+        assert self.operator in {'union',
+                                 'intersection'}, "ERROR: Only 'union' and 'intersection' operators are supported."
+
+    def __set_uuid(self):
+        """Creates a uuid using the class attribute"""
+        inst_uuid = "CQ_" + str(ComplexQuery.UUID_INDEX).zfill(3)
+        ComplexQuery.UUID_INDEX += 1
+        self.UUID = inst_uuid
+
+    def __load_objects(self, keys):
+        """Load the specified dependencies from the shelf files"""
+        # assert path.exists(shelf_path), "ERROR: the specified path does not exist."   # todo, get working
+        items = []
+        with shelve.open(self.shelf_path, 'r') as shelf:
+            for key in keys:
+                assert key in shelf.keys(), "ERROR: one or more of the keys specified is not on the shelf."
+                items.append(shelf[key])
+        return items
+
+    def update_dependencies(self, key_list):
+        """Updates the instance flag dictionary"""
+        self.processed = False  # if previously processed, set to False since instance has been changed
+        self.operator = None  # if previously processed, set to None since instance has been changed
+        self._dependencies = self.__load_objects(key_list)
+        # todo reset phrase i.e. "union(SQ_001, SQ_002)"
+
+    def update_operator(self, operator):
+        """TODO"""
+        pass
+
+    def apply_query(self, file_set):
+        """Interface method call for applying a query to the instance and getting a match"""
+        assert type(file_set) is set, "ERROR: file_set must be of type <class 'set'>"
+
+        if self.operator == 'union':
+            self.union_query(file_set)
+        else:
+            self.intersection_query(file_set)
+        self.processed = True
+
+    def union_query(self, file_set):
+        """Computes the union of the dependency matches"""
+        self.match = dict()
+        for dependency in self._dependencies:
+            # dependencied should be in order of their composition
+            # |-> (i.e. no complex query should come before all its constituent queries)
+            if not dependency.processed:
+                dependency.apply_query(file_set)
+            # print("Name:", dependency.UUID, "Type:", dependency.type)
+            # print(dependency.match)
+            for file, pages in dependency.match.items():
+                if file not in self.match.keys():
+                    self.match[file] = set(pages)
+                else:
+                    self.match[file].update(set(pages))
+
+        # Convert pages in match back to list for continuity and order
+        for file in self.match:
+            self.match[file] = sorted(list(self.match[file]))
+
+    def intersection_query(self, file_set):
+        """Computes the intersection of the dependency matches (union of pages that match intersection)"""
+        self.match = dict()
+        for dependency in self._dependencies:
+            # dependencied should be in order of their composition
+            # |-> (i.e. no complex query should come before all its constituent queries)
+            if not dependency.processed:
+                dependency.apply_query(file_set)
+
+        # Get set_list for matches
+        set_list = [set(dependency.match.keys()) for dependency in self._dependencies]
+        intersection = set.intersection(*set_list)
+
+        # For each file that matches the intersection operator, take the union of the pages
+        # todo take intersection of pages
+        for file in intersection:
+            pages = sorted(
+                list(set(chain.from_iterable([dependency.match[file] for dependency in self._dependencies]))))
+            self.match[file] = pages
